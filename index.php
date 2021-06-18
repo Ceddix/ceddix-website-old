d17f55ff5fbdb88f38636ab94b285545dfb4c3a2
Ceddix <seciccedrik@gmail.com>
2021-06-18 13:39:38 +0200

    changed url/website routing

--- index.php
+++ index.php
@@ -4,23 +4,22 @@
 $content = file_get_contents("redirects.json");
 $redirects = json_decode($content, true);
 
-// Checks if ID exists in custom links
-if (isset($redirects[$_GET["id"]])) {
+// ID is set AND redirect is SET
+if (isset($_GET["id"]) && isset($redirects[$_GET["id"]])) {
     header("location: " . $redirects[$_GET["id"]]);
 }
-// Checks there is an file named like ID
+
+// (else) FILE "%ID%.php" exists
 elseif (@file_exists($_GET['id'] . '.php')) {
     include $_GET['id'] . ".php";
 }
-// Checks if a empty ID is set => 404
-elseif (isset($_GET["id"]) && !empty($_GET["id"])) {
+
+// (else) ID IS SET AND NOT EMPTY
+elseif (isset($_GET["id"]) && !empty($_GET['id'])) {
     include "errorpages/404.php";
 }
-// Checks if a ID is set, but there isn't any custom link or file => 404
-elseif (!empty($_GET["id"])) {
-    include "errorpages/404.php";
-}
-// Else => Homepage
+
+// else => Homepage
 else {
     include "main.php";
 }
