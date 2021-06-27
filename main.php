e286b907f051b41bc9b2383aeb452518c0d7c7b9
Ceddix <seciccedrik@gmail.com>
2021-06-27 21:32:54 +0200

    set link target to blank

--- main.php
+++ main.php
@@ -24,42 +24,42 @@
             </div>
              <!-- SOCIAL MEDIA -->
              <div class="main-social">
-                 <a href="https://github.com/Ceddix" class="social-github social-tooltip">
+                 <a href="https://github.com/Ceddix" target="_blank" class="social-github social-tooltip">
                      <i class="fab fa-github"></i>
                      <span class="social-tooltiptext" style="width: 84px; margin-left: -42px">
                          <strong>GitHub</strong><br>
                          <span style="color: #5b5e64">@Ceddix</span>
                      </span>
                  </a>
-                 <a href="https://www.reddit.com/user/CeddixCed" class="social-reddit social-tooltip">
+                 <a href="https://www.reddit.com/user/CeddixCed" target="_blank" class="social-reddit social-tooltip">
                      <i class="fab fa-reddit"></i>
                      <span class="social-tooltiptext" style="width: 110px; margin-left: -55px">
                          <strong>Reddit</strong><br>
                          <span style="color: #5b5e64">u/CeddixCed</span>
                      </span>
                  </a>
-                 <a href="https://steamcommunity.com/id/ceddix/" class="social-steam social-tooltip">
+                 <a href="https://steamcommunity.com/id/ceddix/" target="_blank" class="social-steam social-tooltip">
                      <i class="fab fa-steam"></i>
                      <span class="social-tooltiptext" style="width: 110px; margin-left: -55px">
                          <strong>Steam</strong><br>
                          <span style="color: #5b5e64">@CeddixCed</span>
                      </span>
                  </a>
-                 <a href="https://discord.com/invite/FGCq5GN" class="social-discord social-tooltip">
+                 <a href="https://discord.com/invite/FGCq5GN" target="_blank" class="social-discord social-tooltip">
                      <i class="fab fa-discord"></i>
                      <span class="social-tooltiptext" style="width: 75px; margin-left: -37.5px">
                          <strong>Discord</strong><br>
                          <span style="color: #5b5e64">Ceddix</span>
                      </span>
                  </a>
-                 <a href="https://www.instagram.com/ceddix/" class="social-instagram social-tooltip">
+                 <a href="https://www.instagram.com/ceddix/" target="_blank" class="social-instagram social-tooltip">
                      <i class="fab fa-instagram"></i>
                      <span class="social-tooltiptext" style="width: 90px; margin-left: -45px">
                          <strong>Instagram</strong><br>
                          <span style="color: #5b5e64">@ceddix</span>
                      </span>
                  </a>
-                 <a href="https://twitter.com/CeddixCed/" class="social-twitter social-tooltip">
+                 <a href="https://twitter.com/CeddixCed/" target="_blank" class="social-twitter social-tooltip">
                      <i class="fab fa-twitter"></i>
                      <span class="social-tooltiptext" style="width: 110px; margin-left: -55px">
                          <strong>Twitter</strong><br>
