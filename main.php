5bc85273a426c69c545e55e20ab8ce919738e209
Ceddix <seciccedrik@gmail.com>
2021-06-29 00:54:28 +0200

    added tooltip fadeout

--- main.php
+++ main.php
@@ -24,44 +24,44 @@
             </div>
              <!-- SOCIAL MEDIA -->
              <div class="main-social">
-                 <a href="https://github.com/Ceddix" target="_blank" class="social-github social-tooltip">
+                 <a href="https://github.com/Ceddix" target="_blank" class="social-github social-tooltip" onmouseleave="tooltipFadeOut('social-github')" onmouseover="tooltipFadeIn('social-github')">
                      <i class="fab fa-github"></i>
-                     <span class="social-tooltiptext" style="width: 84px; margin-left: -42px">
+                     <span class="social-tooltiptext" style="width: 84px; margin-left: -42px" id="social-github">
                          <strong>GitHub</strong><br>
                          <span style="color: #5b5e64">@Ceddix</span>
                      </span>
                  </a>
-                 <a href="https://www.reddit.com/user/CeddixCed" target="_blank" class="social-reddit social-tooltip">
+                 <a href="https://www.reddit.com/user/CeddixCed" target="_blank" class="social-reddit social-tooltip" onmouseleave="tooltipFadeOut('social-reddit')">
                      <i class="fab fa-reddit"></i>
-                     <span class="social-tooltiptext" style="width: 110px; margin-left: -55px">
+                     <span class="social-tooltiptext" style="width: 110px; margin-left: -55px" id="social-reddit">
                          <strong>Reddit</strong><br>
                          <span style="color: #5b5e64">u/CeddixCed</span>
                      </span>
                  </a>
-                 <a href="https://steamcommunity.com/id/ceddix/" target="_blank" class="social-steam social-tooltip">
+                 <a href="https://steamcommunity.com/id/ceddix/" target="_blank" class="social-steam social-tooltip" onmouseleave="tooltipFadeOut('social-steam')">
                      <i class="fab fa-steam"></i>
-                     <span class="social-tooltiptext" style="width: 110px; margin-left: -55px">
+                     <span class="social-tooltiptext" style="width: 110px; margin-left: -55px"id="social-steam">
                          <strong>Steam</strong><br>
                          <span style="color: #5b5e64">@CeddixCed</span>
                      </span>
                  </a>
-                 <a href="https://discord.com/invite/FGCq5GN" target="_blank" class="social-discord social-tooltip">
+                 <a href="https://discord.com/invite/FGCq5GN" target="_blank" class="social-discord social-tooltip" onmouseleave="tooltipFadeOut('social-discord')">
                      <i class="fab fa-discord"></i>
-                     <span class="social-tooltiptext" style="width: 75px; margin-left: -37.5px">
+                     <span class="social-tooltiptext" style="width: 75px; margin-left: -37.5px" id="social-discord">
                          <strong>Discord</strong><br>
                          <span style="color: #5b5e64">Ceddix</span>
                      </span>
                  </a>
-                 <a href="https://www.instagram.com/ceddix/" target="_blank" class="social-instagram social-tooltip" id="social-tooltip" onmouseover="tooltipAnimation()">
+                 <a href="https://www.instagram.com/ceddix/" target="_blank" class="social-instagram social-tooltip" onmouseleave="tooltipFadeOut('social-instagram')">
                      <i class="fab fa-instagram"></i>
-                     <span class="social-tooltiptext" style="width: 90px; margin-left: -45px">
+                     <span class="social-tooltiptext" style="width: 90px; margin-left: -45px" id="social-instagram">
                          <strong>Instagram</strong><br>
                          <span style="color: #5b5e64">@ceddix</span>
                      </span>
                  </a>
-                 <a href="https://twitter.com/CeddixCed/" target="_blank" class="social-twitter social-tooltip"  id="social-tooltip" onmouseover="tooltipAnimation()">
+                 <a href="https://twitter.com/CeddixCed/" target="_blank" class="social-twitter social-tooltip" onmouseleave="tooltipFadeOut('social-twitter')">
                      <i class="fab fa-twitter"></i>
-                     <span class="social-tooltiptext" style="width: 110px; margin-left: -55px">
+                     <span class="social-tooltiptext" style="width: 110px; margin-left: -55px" id="social-twitter">
                          <strong>Twitter</strong><br>
                          <span style="color: #5b5e64">@CeddixCed</span>
                      </span>
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,9 @@
 /* Main */
-
+function tooltipFadeOut(social) {
+    let node = document.getElementById(social);
+    node.style.visibility = "visible";
+    setTimeout(function () {node.style.visibility = "";}, 200)
+}
 
 /* Easter-Eggs */
 function titelreverse() {
